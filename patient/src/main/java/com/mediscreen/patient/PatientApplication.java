7b362be64f6e631fc47545f01fb6dbcec92d8b1a
jedi10 <kyle10_dev@hotmail.fr>
2021-06-15 21:24:21 +0200

    add resource link

--- patient/src/main/java/com/mediscreen/patient/PatientApplication.java
+++ patient/src/main/java/com/mediscreen/patient/PatientApplication.java
@@ -3,7 +3,7 @@ package com.mediscreen.patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#using.using-the-springbootapplication-annotation
 public class PatientApplication {
 
 	public static void main(String[] args) {
