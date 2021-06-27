d606b01b0ec126b34d69f22639ca5bf2606f32a2
jedi10 <kyle10_dev@hotmail.fr>
2021-06-27 13:59:05 +0200

    UI: generate javadoc

---
+++ docs/apidocs/ui/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.mediscreen.ui"},{"l":"com.mediscreen.ui.configuration"},{"l":"com.mediscreen.ui.controller"},{"l":"com.mediscreen.ui.exception"},{"l":"com.mediscreen.ui.model"},{"l":"com.mediscreen.ui.service.restTemplateService"},{"l":"com.mediscreen.ui.tool"},{"l":"com.mediscreen.ui.web.validation"}]
