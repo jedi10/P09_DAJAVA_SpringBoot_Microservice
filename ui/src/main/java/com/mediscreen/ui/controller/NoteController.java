ddf323963f7d546e7815310cb489955f243fab80
jedi10 <kyle10_dev@hotmail.fr>
2021-06-29 19:55:03 +0200

    UI: Connect NoteListPage with RestTemplateService; Show Patient Name
    localMode has been desactivated: List.html page is ok; A link in Patient.List.html has been added to go on NoteList.html page

--- ui/src/main/java/com/mediscreen/ui/controller/NoteController.java
+++ ui/src/main/java/com/mediscreen/ui/controller/NoteController.java
@@ -1,11 +1,15 @@
 package com.mediscreen.ui.controller;
 
+import com.mediscreen.ui.exception.NoteCrudException;
 import com.mediscreen.ui.exception.PatientCrudException;
 import com.mediscreen.ui.model.Note;
 import com.mediscreen.ui.model.Patient;
+import com.mediscreen.ui.service.restTemplateService.NoteRestService;
+import com.mediscreen.ui.service.restTemplateService.PatientRestService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,16 +30,27 @@ import java.util.List;
 @RequestMapping("/note/")
 public class NoteController {
 
-    private static List<Note> noteList = new ArrayList<>();
+    @Autowired
+    private NoteRestService noteRestService;
 
-    private Boolean localMode = true;
+    @Autowired
+    private PatientRestService patientRestService;
+
+    private static List<Note> noteList = new ArrayList<>();
+    private static Patient patient;
+
+    private Boolean localMode = false;
 
     static {
         int patientId = 1;
-        noteList.add(new Note(patientId,"premiere visite au centre medical; injection vaccin", LocalDate.now()));
+        noteList.add(new Note(patientId,"premiere visite au centre medical; injection vaccin",
+                LocalDate.now().minus(14, ChronoUnit.DAYS)));
         noteList.get(0).setId("0");
         noteList.add(new Note(patientId, "Seconde visite au centre medical: confirmation injection vaccin", LocalDate.now()));
         noteList.get(1).setId("1");
+        patient = new Patient("M","Dmitri","Gloukhovski",
+                LocalDate.of(1979,6,12),"Moscou","phone1_Test");
+        patient.setId(patientId);
     }
 
     @GetMapping(value = "{patientId}/list")
@@ -43,16 +59,20 @@ public class NoteController {
         log.info("UI: Get Note List on URL: '{}' : RESPONSE STATUS: '{}'",
                 request.getRequestURI(),
                 response.getStatus());
+
         if (localMode){
+            model.addAttribute("patient", NoteController.patient);
             model.addAttribute("notes", NoteController.noteList);
-        } /**else {
+        } else {
             try {
-                List<Note> notes = noteRestService.getList();
+                Patient patient = patientRestService.getById(patientId);
+                model.addAttribute("patient", patient);
+                List<Note> notes = noteRestService.getList(patientId);
                 model.addAttribute("notes", notes);
-            } catch (NoteCrudException e) {
+            } catch (NoteCrudException | PatientCrudException e) {
                 model.addAttribute("errorListingNotes", e.getMessage());
             }
-        }**/
+        }
         return "note/list";
     }
 }
